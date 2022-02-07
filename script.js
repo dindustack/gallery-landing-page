50f163f819fc4e2087197df2f7fe1a566b301579
dindustack <silviachinwendu@gmail.com>
2022-02-07 21:51:45 +0100

    final cimmit

---
+++ script.js
@@ -0,0 +1,9 @@
+const tl = gsap.timeline()
+tl.from(".img, .heading, .menu, h1, p, .bottom-section", {
+    duration: 1,
+    y: 200,
+    opacity: 0,
+    stagger: {
+        amount: 0.5
+    }
+})
